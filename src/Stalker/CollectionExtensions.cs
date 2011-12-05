439f45c007f1b66af60d8f0e39b58216a84aad4c
Damian Reeves <damreev@gmail.com>
2011-12-05 17:15:19 -0500

    Updates

---
+++ src/Stalker/CollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace Stalker {
+    public static class CollectionExtensions {
+        public static IObservable<NotifyCollectionChangedEventArgs> ObserveCollectionChanged(this IEnumerable source) {
+            var ncc = source as INotifyCollectionChanged;
+            if (ncc != null) {
+                return Observable.FromEventPattern<NotifyCollectionChangedEventHandler, NotifyCollectionChangedEventArgs>(
+                    h => ncc.CollectionChanged += h,
+                    h => ncc.CollectionChanged -= h
+                ).Select(ev =>ev.EventArgs);
+            }
+            return Observable.Never<NotifyCollectionChangedEventArgs>();
+        }
+
+        public static IObservable<object> ObserveCollectionItemChanged(this IEnumerable source) {
+            throw new NotImplementedException();
+        }
+    }
+
+    [Flags]
+    public enum ChangeType {
+        None = 0,
+        ValueChanged = 1,
+        IsReadOnly = 2,
+        ItemAdded = 4,
+        ItemRemoved = 8,
+        ItemReplaced = ItemRemoved | ItemAdded,
+        CollectionReset = 16,
+    }
+
+    public interface IChangeNotification<out TSender, out TValue> {
+        TSender Sender { get; }
+        TValue Value { get; }
+        string PropertyName { get; }
+    }
+
+    public class ChangeNotification<TSender,TValue>: IChangeNotification<TSender,TValue> {
+        public TSender Sender { get; set; }
+        public TValue Value { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
--- src/Stalker/Stalker.cs
+++ src/Stalker/Stalker.cs
@@ -1,9 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
 
 namespace Stalker {
-    public class Stalker {
+    public static class Stalker {        
+        public static IStalker Stalk<T>(T watched) {
+            return new Stalker<T>();
+        }
+
+        private static void Stalk(INotifyCollectionChanged collection) {            
+            
+        }        
     }
+
+    internal class Stalker<T> : IStalker {
+        private readonly CompositeDisposable _disposable = new CompositeDisposable();
+        public void Stalk(INotifyCollectionChanged collection) {
+
+        }
+
+        public IStalker Watch<TResult>(Expression<Func<T,TResult>> propertyExpression) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            if (!_disposable.IsDisposed) {
+                _disposable.Dispose();
+            }
+        }
+    }
+
+    public interface IStalker: IDisposable {
+        
+    }
+
+    
 }
